72bd47d573893c0b0eb48a6baa72fd77dd5c96da
nit3owl <nit3owldev@gmail.com>
2017-10-31 22:33:58 -0700

    Add Go solution

---
+++ src/go/fizz-buzz.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+import "strconv"
+
+func main() {
+    for i := 1; i <= 100; i++ {
+        result := ""
+        
+        if(i % 3 == 0) {
+            result += "Fizz"
+        }
+
+        if(i % 5 == 0) {
+            result += "Buzz"
+        }
+
+        if(result == "") {
+            result = strconv.Itoa(i);
+        }
+
+        fmt.Println(result);    
+    }    
+}
+
