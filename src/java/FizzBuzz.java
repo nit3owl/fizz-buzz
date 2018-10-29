f69ecd3f7c6a146c5a78391c23dad3f5362e0708
nit3owl <nit3owldev@gmail.com>
2018-10-28 21:14:54 -0700

    Add a constant type to the default calculator

--- src/java/FizzBuzz.java
+++ src/java/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz{
     
     public static void main(String[] args){
         FizzBuzzCalculatorFactory fizzBuzzCalculatorFactory = new FizzBuzzCalculatorFactory();
-        FizzBuzzCalculator fizzBuzzCalculator = fizzBuzzCalculatorFactory.getCalculator("fizzBuzz");
+        FizzBuzzCalculator fizzBuzzCalculator = fizzBuzzCalculatorFactory.getCalculator(DefaultFizzBuzzCalculator.TYPE);
 
         List<String> fizzedBuzzes = fizzBuzzCalculator.calculate(100);
         fizzedBuzzes.forEach(System.out::println);
